7d7e50bbefa46f3a4d6f0c7a43c30b59c6bffee5
Kala Foods <guhan.delta@gmail.com>
2021-09-25 22:04:04 -0400

    completed the backedn server configurations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')(); // Calling teh Express fn once more to givet the Express App
 const server = require('http').createServer(app);
 const cors = require('cors');
+const { Socket } = require( 'socket.io' );
 
 const io = require('socket.io')(server, {
     cors:{
@@ -13,6 +14,21 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => { res.send('Server is running....') })
+app.get('/', (req, res) => { res.send('Server is running....') });
+
+io.on('connection', (req, res) => {
+
+    socket.emit('me', socket.id); //Emits the user ID to the UI once the connection is successful
+
+    socket.on('disconnected', ()=> socket.broadcast.emit('Call ended.....'));
+
+    socket.on("çalluser", ({ userToCall, signalData, from, name })=>{
+        io.to(userToCall).emit("calluser",{ signal: signalData, from, name });
+    });
+
+    socket.on("answerçall", (data)=>{
+        io.to(data.to).emit("callaccepted", data.signal);
+    });
+});
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
