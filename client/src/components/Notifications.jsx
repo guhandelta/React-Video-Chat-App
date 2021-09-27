0de22aa4817caf700bd9594343194526b52b9904
Kala Foods <guhan.delta@gmail.com>
2021-09-26 22:46:05 -0400

    Created basic app structure

---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,11 @@
+
+
+const Notifications = () => {
+    return (
+        <div>
+            <h3>Notifications</h3>
+        </div>
+    )
+}
+
+export default Notifications
---
+++ client/src/components/Options.jsx
@@ -0,0 +1,12 @@
+
+
+const Options = ({ children }) => {
+    return (
+        <div>
+            <h3>Options</h3>            
+            { children }
+        </div>
+    )
+}
+
+export default Options
