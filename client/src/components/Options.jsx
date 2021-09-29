2c10a8193b4df4f0cd0f9c170710962ff3340a7a
Kala Foods <guhan.delta@gmail.com>
2021-09-29 19:16:01 -0400

    Completed the Options component with all the options to make a call

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -45,26 +45,44 @@ const Options = ({ children }) => {
 
     return (
 
-
-        <Container className={classes.container}> 
-            <Paper elevation={10} className={classes.paper}>
-                <form className={classes.root} noValidate autoComplete="off">
-                    <Grid container className={classes.gridContainer}>
-                        <Grid item xs={12} md={6} className={classes.padding}>
-                            <Typography gutterBottom variant="h5">Account Info</Typography>
-                            <TextField label="name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-                            <CopyToClipboard text={me} className={classes.margin}>
-                                <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
-
-                                </Button>
-                            </CopyToClipboard>
+        <>
+            <Container className={classes.container}> 
+                <Paper elevation={10} className={classes.paper}>
+                    <form className={classes.root} noValidate autoComplete="off">
+                        <Grid container className={classes.gridContainer}>
+                            <Grid item xs={12} md={6} className={classes.padding}>
+                                <Typography gutterBottom variant="h5">Account Info</Typography>
+                                <TextField label="name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+                                <CopyToClipboard text={me} className={classes.margin}>
+                                    <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
+                                        Copy your ID
+                                    </Button>
+                                </CopyToClipboard>
+                            </Grid>
+                            <Grid item xs={12} md={6} className={classes.padding}>
+                                <Typography gutterBottom variant="h5">Make a call</Typography>
+                                <TextField label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
+                                {/* The button varies as per the status of the call */}
+                                {callAccepted && !callEnded ? (
+                                    <Button variant="contained" color="secondary" fullWidth startIcon={<PhoneDisabled fontSize="large" />} className={classes.margin} onClick={leaveCall} >
+                                        Hang up
+                                    </Button>
+                                ):(
+                                    <Button variant="contained" color="primary" fullWidth startIcon={<Phone fontSize="large" />} className={classes.margin} onClick={() => callUser(idToCall)} 
+                                        // ->() is used so the callback() gets triggerred only after clicking the button, else the callback will be invoked-
+                                        //- on component load as JS will find it as a function call
+                                    >
+                                        Call
+                                    </Button>
+                                )}
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form>
-            </Paper>
-            <h3>Options</h3>            
-            { children }
-        </Container>
+                    </form>
+                </Paper>
+                <h3>Options</h3>            
+                { children }
+            </Container>
+        </>
     )
 }
 
